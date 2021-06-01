1cd6235bf943f460fbc29428023d8d212acc7fa5
Max Huijssen <max.huijssen@icloud.com>
2021-06-01 17:45:41 +0200

    asdasd

--- MyApplication.cs
+++ MyApplication.cs
@@ -5,8 +5,35 @@ namespace Template
 {
     struct ray
     {
-
+		Vector3 O;	//origin
+		Vector3 D;	//direction
+		float t;	//distance
     }
+
+	class camera
+	{
+		public Vector3 E;
+		public float fov;
+		public Vector3 p0 = new Vector3(1, 1, 0);
+		public Vector3 p1 = new Vector3(-1, 1, 0);
+		public Vector3 p2 = new Vector3(1, -1, 0);
+
+		public camera(Vector3 e, float fieldofview)
+		{
+			E = e;				//camera position
+			fov = fieldofview;  //field of view
+
+		}
+
+		public Vector3 GetP(float u, float v)
+        {
+			Vector3 P;
+			P = p0 + u * (p1 - p0) + v * (p2 - p0);
+			return P;
+        }
+
+	}
+
 	class MyApplication
 	{
 		// member variables
