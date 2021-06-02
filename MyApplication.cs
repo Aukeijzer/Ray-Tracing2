997754df24d4bf17a521297a7aaa25e5c03eccd7
DESKTOP-JSE046R\auker <auke.roest01@gmail.com>
2021-06-03 00:49:55 +0200

    cu,

--- MyApplication.cs
+++ MyApplication.cs
@@ -9,8 +9,8 @@ namespace Template
 		// member variables
 		public Surface screen, debugScreen, rtScreen;
 		public camera camera;
-		public bool debug=true; //determines if the debugscreen will be shown
-		// initialize
+		public bool debug = true; //determines if the debugscreen will be shown
+								  // initialize
 		Surface map;
 		scene scene = new scene();
 		//public Vector3[] pixelsRay;
@@ -38,42 +38,30 @@ namespace Template
 		// tick: renders one frame
 		public void Tick()
 		{
-			//rtScreen.drawscene(scene);
-			//rtScreen.CopyTo(screen, 0, 0);
-			//screen.drawscene(scene);
-			screen.drawdebug(scene);
+			rtScreen.drawscene(scene);
+			rtScreen.CopyTo(screen, 0, 0);
 			if (debug)
-            {
-				//debugScreen.drawdebug(scene,pixelsRayAll,scene.primitives);
-				//debugScreen.CopyTo(screen, 0, 0);
-			}
-			for (int x = 0; x < 256; x++)
 			{
-				//screen.Line(x, 0, x, 512, 0x0000ff);
+				debugScreen.drawdebug(scene);
+				debugScreen.CopyTo(screen, 512, 0);
 			}
-			/*for (int i=0;i<512*512;i++)
-			{
-				Vector3 hdr = pixels[i];
-				rtScreen.pixels[i] = MixColor(f2intcolor(hdr.X), f2intcolor(hdr.Y), f2intcolor(hdr.Z));
-			}*/
-
 		}
-	int TX(float x)
-	{
-		x += 2;
-		return (int)(x / 4 * screen.width);
-	}
-	int TY(float y)
-	{
-		y = -y;
-		y += 2;
-		return (int)(y / 4 * screen.height) * (screen.width / screen.height);
-	}
-	public void RenderGL()
-	{
-			//screen.drawdebug(scene, pixelsRayAll, scene.primitives);
+		int TX(float x)
+		{
+			x += 2;
+			return (int)(x / 4 * screen.width);
+		}
+		int TY(float y)
+		{
+			y = -y;
+			y += 2;
+			return (int)(y / 4 * screen.height) * (screen.width / screen.height);
+		}
+		public void RenderGL()
+		{
+
 		}
 
 
-}
+	}
 }
--- template.cs
+++ template.cs
@@ -40,7 +40,7 @@ namespace Template
 		{
 			// called during application initialization
 			GL.Hint( HintTarget.PerspectiveCorrectionHint, HintMode.Nicest );
-			ClientSize = new Size( 1024, 1024 );
+			ClientSize = new Size( 1024, 512 );
 			app = new MyApplication();
 			app.screen = new Surface( Width, Height );
 			Sprite.target = app.screen;
