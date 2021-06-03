56a7e3ddc46bf98f830ae38877e7a9940eddbdb5
Max Huijssen <max.huijssen@icloud.com>
2021-06-03 14:27:28 +0200

    scene netjes

--- scene.cs
+++ scene.cs
@@ -7,11 +7,13 @@ using OpenTK;
 
 namespace Template
 {
+	//Struct for the rays. WE always use .Normalized() to keep the given direction at length 1, so t will always be it's length.
 	public struct ray
 	{
-		public Vector3 O;  //origin
-		public Vector3 D;  //direction
-		public float t;    //distance
+		public Vector3 O;  //The ray its origin.
+		public Vector3 D;  //The ray its direction.
+		public float t;    //The distance it will or has traveled.
+
 		public ray(Vector3 origin, Vector3 direction)
         {
 			O = origin;
@@ -25,72 +27,82 @@ namespace Template
 			t = distance;
 		}
 	}
-	//The camera class is used to let surfaces know which
+
+	//The camera class is used to set the screenplane, it's corners, the FOV, viewdirection and camera position.
 	public class camera
 	{
-		public Vector3 E;   //camera position
-		public Vector3 V;   //camera direction
-		public Vector3 C;   //screen center
-		public Vector3 p0;  //screen corner (top left)
-		public Vector3 p1;  //screen corner (top right)
-		public Vector3 p2;  //screen corner (bottom left)
-		public Vector3 p3;  //screen corner (bottem right)
-		public float d;     //distance E to screenplane
-		public float fov;
+		public Vector3 E;   //Camera position.
+		public Vector3 V;   //Camera direction (0,0,1) ussually.
+		public Vector3 C;   //Screen center.
+		public Vector3 p0;  //Screen corner (top left).
+		public Vector3 p1;  //Screen corner (top right).
+		public Vector3 p2;  //Screen corner (bottom left).
+		public Vector3 p3;  //Screen corner (bottem right).
+		public float d;     //Distance E to screenplane.
+		public float fov;   //Field of view (degrees).
 
+		//Transform the camera by multiplying p0, p1, p2, and E with the camera matrix.
 		public camera(Vector3 e, int fieldofview, Vector3 viewdirection)
 		{
-			E = e;              //camera position
+			E = e;
 			fov = (float)((fieldofview * Math.PI) / 180);
-			d = (float)(1f / Math.Tan(fov/2)); //distance from E to screen plane (1f is half the screen plane).
-			V = viewdirection.Normalized();  //camera direction (0,0,1)
+			d = (float)(1f / Math.Tan(fov/2));
+			V = viewdirection.Normalized();
 			C = E + d * V;
 			p0 = C + new Vector3(-1, 1, 0);
 			p1 = C + new Vector3(1, 1, 0);
 			p2 = C + new Vector3(-1, -1, 0);
 			p3 = C + new Vector3(1, -1,0);
-		}   //transform camera by multiplying p0, p1, p2, and E with the camera matrix.
-
+		}   
 	}
-	//this scene stores all lightsources and primitives and is used to calculate intersections between rays and primitives. 
+
+	//The scene stores all lightsources and primitives and is used to calculate intersections between rays and primitives. 
 	public class scene
 	{
-		public List<primitive> primitives = new List<primitive>(); //a list of all primitives in the scene
-		public List<light> lightSources = new List<light>(); //a list of all lightsources in the scene
-		public camera camera; //the camera, mainly used as membervariable to pass on to methods using the scene object
-		int maxreflect;
-		//TODO: make less stupid constructor
+		public List<primitive> primitives = new List<primitive>();	//A list of all primitives in the scene.
+		public List<light> lightSources = new List<light>();		//A list of all lightsources in the scene
+		public camera camera;										//The camera, mainly used as membervariable to pass on to methods using the scene object
+		int maxReflect;												//Integer used to show how many reflections are allowed between reflective primitives.
+		
+		//Scene constructor function.
 		public scene()
 		{
 			camera = new camera(new Vector3(0, 0, 0), 120, new Vector3(0, 0, 1));
-			maxreflect = 10000;
+			
 			sphere s1 = new sphere(new Vector3(-3f, 0, 6f), 1f, new Vector3(1,0,0));
 			sphere s2 = new sphere(new Vector3(0, 0, 6f), 1f);
 			sphere s3 = new sphere(new Vector3(3f, 0, 6f), 1f, new Vector3(0, 0, 1));
 			plane p1 = new plane(new Vector3(0, 1, 0), -2);
 			light l1 = new light(new Vector3(9, 9, 1), 1f);
 			light l2 = new light(new Vector3(-9, 6, 2), 0.6f);
+
 			lightSources.Add(l1);
 			lightSources.Add(l2);
 			primitives.Add(s1);
 			primitives.Add(s2);
 			primitives.Add(s3);
 			primitives.Add(p1);
+			maxReflect = 10000;
 		}
-		//for a given ray gives the nearest intersection in a scene with a primitive. returns this as a intersection object.
+
+		//For a given ray gives the nearest intersection in a scene with a primitive. Returns this as n intersection object.
 		public intersect calcIntersection(ray r,int i)
 		{
-			//if this is true then the ray met a primitive
+			//If true, then the ray met a primitive.
 			bool intersection=false;
-			//the object with the nearest intersection
+
+			//The object with the nearest intersection.
 			primitive obj=new primitive();
+
 			Vector3 D = r.D;
 			Vector3 O = r.O;
-			//t is calculated as the distance between the origin of the ray and the intersection
+
+			//t is calculated as the distance between the origin of the ray and the intersection.
 			float t=1e37f;
+
 			foreach (primitive P in primitives)
 			{
-				//calculating intersections for all spheres in the scene
+				//Calculating intersections for all spheres in the scene.
 				if (P is sphere)
 				{
 					sphere s = (sphere)P;
@@ -100,10 +112,11 @@ namespace Template
 					float b = 2*Vector3.Dot(D, SO);
 					float c = Vector3.Dot(SO, SO) - rad * rad;
 					float d = b * b - 4 * a * c;
-					//if d<0 then the sphere has no intersection with the ray
+
+					//If d<0 then the sphere has no intersection with the ray.
 					if (d >= 0)
 					{
-						//normally t=t1, however in the edge case where te camera is inside the sphere the t=t2
+						//Normally t=t1, however in the edge case where te camera is inside the sphere, t=t2.
 						float t1= -(b + (float)Math.Sqrt(d)) / (2 * a);
 						if (t1 >= 0)
                         {
@@ -121,30 +134,29 @@ namespace Template
 						}
 					}
 				}
-				//calculating intersections for all planes in the scene
+
+				//Calculating intersections for all planes in the scene.
 				if (P is plane)
                 {
 					plane p = (plane)P;
 					Vector3 N = p.normal;
 					float d = p.t;
 					float ndotd = Vector3.Dot(N, D);
-					//if ndot==0 then the ray is parallel to the plane
+
+					//If ndot==0 then the ray is parallel to the plane.
 					if (ndotd == 0)
 					{
-						//check if the origin is in the plane(which means N* O+d == 0) and if it is t is set to 0.
+						//Check if the origin is in the plane (which means N*O+d == 0). If it is, t is set to 0.
 						if (Vector3.Dot(N, O) == 0)
                         {
 							intersection = true;
 							t = 0;
 						}
 					}
-					//if ndot!=0 calculate the intersection the regular way
+
+					//If ndot!=0 calculate the intersection the regular way.
 					else
 					{
-						if (r.D.Y > 0)
-                        {
-							int cum = 69;
-                        }
 						float t1 = -(Vector3.Dot(N,O) + d) / ndotd;
 						if (t1 >= 0)
                         {
@@ -161,27 +173,29 @@ namespace Template
 			}
 			intersect intersect = new intersect(r);
 			Vector3 point = r.O + r.t * r.D;
-			//if the primitive is reflective the ray will reflect and land somewhere else, thus caclIntersection will be call recursively to find the intersection of that ray.
+
+			//If the primitive is reflective the ray will reflect and land somewhere else, thus caclIntersection will be call recursively to find the intersection of that ray.
 			if (obj.reflective)
 			{
-				//if the ray has reflected maxreflect amount of times and hits a reflective object it will be assumed the ray didnt meet a non-reflective primitive.
-				if (i < maxreflect)
+				//If the ray has reflected maxReflect amount of times and hits a reflective object it will be assumed the ray didnt meet a non-reflective primitive.
+				if (i < maxReflect)
 				{
-					//calculating the normal to the surface the ray reflects upon
+					//Calculating the normal to the surface the ray reflects upon.
 					Vector3 normal = new Vector3();
 					if (obj is sphere) normal = (point - ((sphere)obj).pos).Normalized();
 					if (obj is plane) normal = ((plane)obj).normal.Normalized();
 
-					//calculating the direction of the reflected ray
+					//Calculating the direction of the reflected ray.
 					Vector3 D2 = r.D - 2 * Vector3.Dot(r.D, normal) * normal;
 					ray r2 = new ray(point, D2);
-					//calling calcIntersection to see where the new ray will end up
+
+					//Calling calcIntersection to see where the new ray will end up.
 					intersect = calcIntersection(r2, i + 1);
 				}
 			}
             else
             {
-				//if the ray met a primitive who isnt reflective it will
+				//Creates an intersect object that stores the point of intersection, the object it insects with, and the intersecting ray.
 				if (intersection) { 
 					intersect = new intersect(point, obj, r); 
 				}
@@ -189,17 +203,19 @@ namespace Template
 			return intersect;
 		}
 	}
-	//the class intersection is used to store information about intersections of rays and to handle logic with intersections.
+
+	//The class intersect is used to store information about intersections of rays and to handle logic with intersections.
 	public class intersect
     {
-		public Vector3 point; //this is the point where the ray lands
-		public primitive obj; //the primitive with which is intersected
-		public ray r; //the ray which caused the intersection
-		public bool intersection_made; //if this is true then the ray has met an non-reflective object (when a ray doesnt meet any primitives still creates an intersection object)
-		//if an intersection is made between a ray and primitive this constructormethod will store all relevant information in the membervariables.
+		public Vector3 point;					//The the point where the ray lands.
+		public primitive obj;					//The primitive with which is intersected.
+		public ray r;							//The ray which caused the intersection.
+		public bool intersection_made;			//If this is true then the ray has met an non-reflective object (when a ray doesnt meet any primitives it will still create an intersect object).
+
+		//If an intersection is made between a ray and primitive this constructor method will store all relevant information in the membervariables.
 		public intersect(Vector3 point,primitive obj, ray r)
 	    {
-			//if the color is zero obj is just an empty intitialized obj
+			//If the color is zero, obj is just an empty intitialized obj.
 			if (obj.rgbcolor!=new Vector3(0, 0, 0))
             {
 				this.point = point;
@@ -213,7 +229,8 @@ namespace Template
 				this.intersection_made = false;
             }
         }
-		//if no intersection is made for a ray this constructor is used.
+
+		//If no intersection is made for a ray this constructor is used instead.
 		public intersect(ray r)
         {
 			this.r = r;
